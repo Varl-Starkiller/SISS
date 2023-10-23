73ee89504bb775263df7f30a98ea118a82aae542
Gyan Hari Dahal <dgyanhari@gmail.com>
2023-10-23 22:16:26 +0545

    Updated Login site along with its javascript and stylesheet

--- static/js/login.js
+++ static/js/login.js
@@ -1,8 +1,22 @@
-document.addEventListener("DOMContentLoaded", () =>{
-    const loginInputs = document.querySelectorAll('.login-input-parent input')
-    loginInputs.forEach((inputBox) =>{
-        if(inputBox.value !==""){
-            inputBox.classList.toggle('active-label')
+const loginInputs = document.querySelectorAll('.login-input-parent input');
+loginInputs.forEach((inputBox) =>{
+    inputBox.addEventListener('input', ()=>{
+        const label = inputBox.previousElementSibling;
+        if(inputBox.value){
+            label.classList.add('active-label');
+            inputBox.style.borderColor = "#0038FF"
+            inputBox.style.caretColor = "#000000"
         }
-    })
+        else {
+            label.classList.remove('active-label')
+            inputBox.style.borderColor = "#000000"
+            inputBox.style.caretColor = "#ffffff"
+        }
+    });
+});
+document.querySelector('.sign-in-button').addEventListener('click', ()=>{
+    const email = document.getElementById('email').value;
+    const passwordConfirm = document.getElementById('password').value;
+    console.log(email)
+    console.log(passwordConfirm);
 })
