0e12ba05b82b037be96abcda330b7b1e29f2c978
manish timsina <manishtimsina770319@gmail.com>
2023-10-31 17:53:11 +0545

    added a class to save the data from the signup form

--- auth/models.py
+++ auth/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+class Nuser(models.Model):
+    firstname = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    password = models.CharField(max_length=100)
+
+
+
+    class Meta:
+        app_label = 'auth'  # Specify the app name to which this model belongs
