7cff24528571b3feafbae32c48eec2f5c13ed93c
manish timsina <manishtimsina770319@gmail.com>
2023-10-31 17:54:49 +0545

    added a new function for data insertion in Nuser table

--- auth/views.py
+++ auth/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
 
+from auth.models import Nuser
+
 
 # Create your views here.
-def authe(request):
-    return render(request, 'signuplight.html')
+def signup(request):
+    if request.method == "POST":
+        first_name = request.POST.get('firstname')
+        last_name = request.POST.get('lastname')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        new_user = Nuser(firstname=first_name, lastname=last_name, email=email, password=password)
+        new_user.save()
+    return render(request,  'signuplight.html')
