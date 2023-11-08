c003d5304987fd3946969d6603855d6b131221b6
manish timsina <manishtimsina770319@gmail.com>
2023-11-08 15:56:07 +0545

    added time stamp on sign up

--- auth/models.py
+++ auth/models.py
@@ -7,6 +7,7 @@ class Nuser(models.Model):
     lastname = models.CharField(max_length=100)
     email = models.CharField(max_length=100)
     password = models.CharField(max_length=100)
+    signup_date = models.DateTimeField(auto_now_add=True, db_column='date_joined')
 
     class Meta:
-        app_label = 'auth'  # Specify the app name to which this model belongs
+        app_label = 'auth'   # Specify the app name to which this model belongs
