be54cb13521895a49dc8053787abc27e506e3c84
manish timsina <manishtimsina770319@gmail.com>
2023-11-08 15:56:45 +0545

    added channel in the installed app

--- startup_idea/settings.py
+++ startup_idea/settings.py
@@ -32,13 +32,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'channels',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    
 ]
 
 MIDDLEWARE = [
@@ -135,3 +135,7 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# custom date and time for signup
+#
+
