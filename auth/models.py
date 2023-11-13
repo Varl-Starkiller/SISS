4107fc39920fe47314466753350c3f3c55f51c0c
manish timsina <manishtimsina770319@gmail.com>
2023-11-13 08:33:56 +0545

    added form data insertion in the database

--- auth/models.py
+++ auth/models.py
@@ -2,12 +2,11 @@ from django.db import models
 
 
 # Create your models here.
-class Nuser(models.Model):
+class users(models.Model):
     firstname = models.CharField(max_length=100)
     lastname = models.CharField(max_length=100)
     email = models.CharField(max_length=100)
     password = models.CharField(max_length=100)
-    signup_date = models.DateTimeField(auto_now_add=True, db_column='date_joined')
 
     class Meta:
-        app_label = 'auth'   # Specify the app name to which this model belongs
+        app_label = 'auth'  # Specify the app name to which this model belongs
