c6598175354a11a47f9670b5486dee59b5565107
manish timsina <manishtimsina770319@gmail.com>
2023-11-14 07:05:19 +0545

    can fetch caption and image from database

--- home/models.py
+++ home/models.py
@@ -3,9 +3,11 @@ from django.db import models
 
 # Create your models here.
 
-class Fpost(models.Model):
+class posts(models.Model):
     caption = models.CharField(max_length=1000)
+    image = models.ImageField(upload_to='images/', null=True, default=None)
 
     # image
+
     class Meta:
         app_label = 'home'
