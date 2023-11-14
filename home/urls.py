41bf7714e4eed292b8680031d4cdb3d0d4159c3f
manish timsina <manishtimsina770319@gmail.com>
2023-11-14 07:05:54 +0545

    added urls to home

--- home/urls.py
+++ home/urls.py
@@ -1,9 +1,12 @@
 # from django.contrib import admin
-from django.urls import path, include
-
+from django.urls import path
 from home import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
-    path('post/', views.npost, name='home'),
+    path('post/', views.upload_image, name='home'),
+    path('caption/', views.caption, name='caption'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
